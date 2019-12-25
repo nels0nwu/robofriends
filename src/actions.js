9254ec995224f0c9ccc8ab2203b66aa3b4b7f8d7
Nelson Wu <nelson@nelsonwu.net>
2019-12-25 13:13:46 +1100

    added middleware: redux-logger

--- src/actions.js
+++ src/actions.js
@@ -1,6 +1,5 @@
 import { CHANGE_SEARCH_FIELD } from './constants.js'
 
 export function setSearchField(text) {
-    console.log(text);
     return { type: CHANGE_SEARCH_FIELD, payload: text }
 }
--- src/reducers.js
+++ src/reducers.js
@@ -5,7 +5,6 @@ const initialState = {
 }
 
 function searchRobots(state = initialState, action) {
-    console.log(action);
     switch (action.type) {
         case CHANGE_SEARCH_FIELD:
             return Object.assign({},  state, {searchField: action.payload});
